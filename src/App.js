5c5143a5afe0a467d938fa6b7af9efc701a09a8a
Nels-J <jesus.nels@gmail.com>
2021-05-11 17:33:56 +0200

    Adding new card picture in the gamepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import GalleryPage from './screens/GalleryPage';
+import GamePage from './screens/GamePage';
 // import GalleryPage from "../src/components/"
 
 function App() {
   return (
     <div className="App">
       <div>
-    <GalleryPage />
+    {/* <GalleryPage /> */}
+    <GamePage />
       </div>
     </div>
   );
--- src/screens/GamePage.js
+++ src/screens/GamePage.js
@@ -5,21 +5,20 @@ const GamePage = () => {
   //components of the page and of the game
   return (
           <div>
-    <h1>Jouez !</h1>
-            <div className="bg-image w-full min-h-screen flex flex-wrap justify-center items-center gap-3 py-5">
+             <div className="bg-image w-full min-h-screen flex flex-wrap justify-center items-center gap-3 py-5">
               <div className="backdrop w-10/12 md:w-1/4 bg-white bg-opacity-10 rounded p-3 text-white border border-gray-300 shadow-lg">
                 {/* header */}
                 <div className="w-full mb-3 pb-3 border-b border-1 border-white">
-                  <h3 className="text-xl font-semibold text-shadow">Something Good</h3>
+                  <h3 className="text-xl font-semibold text-shadow">Play Now !</h3>
                 </div>
                 {/* body */}
                 <div>
-                  <img src="https://i.postimg.cc/SxLx0fHV/bg01.jpg" alt="image1" className="w-full h-48 object-cover mb-2">
+                  <img src="https://media.giphy.com/media/WTuPi9GCylyNZ6Wr56/giphy.gif" alt="jigsaw" className="w-full h-48 object-cover mb-2"/>
                   <p className="mb-3 tracking-wide text-base text-shadow">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, omnis.
+                    It's your turn jigsaw guys!
                   </p>
                   <button className="backdrop bg-white bg-opacity-0 border border-white px-3 py-1.5 rounded focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-40 hover:bg-opacity-10 text-lg">
-                    Detail
+                    Back
                   </button>
                 </div>
               </div>
