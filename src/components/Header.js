f8c408d41cbd477313d2d2f0c66957cb68385c7e
BuG-Dev-Joris <maupied69@hotmail.com>
2021-05-12 00:11:42 +0200

    fixed lang of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Header()  {
     return (
         <div className="header-bg bg-cover text-white text-center text-4xl bg-auto text-gray-200" >
-         <h1>Bienvenue sur SpacePuzzle</h1>
+         <h1>Welcome to SpacePuzzle</h1>
          <h2 className="text-2xl">011101100010011110100100100110000101</h2>
          <h4 className="text-xl">( Click on a picture to start the SpacePuzzle )</h4>
          </div>
