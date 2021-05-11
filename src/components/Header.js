b7ce2b07dd5c88bacc4565307038c48da668de75
BuG-Dev-Joris <maupied69@hotmail.com>
2021-05-11 23:25:13 +0200

    fixed header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Header()  {
     return (
-        <div className="header-bg w-100 text-white container mx-auto rounded-t shadow bg-cover bg-bottom m-4" >
-         <h1>Ceci est le Header</h1>
+        <div className="header-bg text-white bg-auto" >
+         <h1>Bienvenue sur 011101001000110</h1>
          </div>
     )
 
