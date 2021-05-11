be68cefba0b1956c74b6f074c6219475af90b9b7
Doriane <berdalado@gmail.com>
2021-05-11 17:43:22 +0200

    clicable card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import "./Card.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({ title, url, id }) {
   return (
     <div className='backdrop w-10/12 md:w-1/4 bg-white bg-opacity-10 rounded p-3 text-white border border-gray-300 shadow-lg'>
-      <div className='w-full mb-3 pb-3 border-b border-1 border-white'>
-        <h3 className='text-xl font-semibold text-shadow'>{title}</h3>
-      </div>
-      <div>
-        <img class='w-full h-48 object-cover mb-2' src={url} alt={title} />
-        <p>{id}</p>
-      </div>
+      <Link to={`./games/${id}`}>
+        <div className='w-full mb-3 pb-3 border-b border-1 border-white'>
+          <h3 className='text-xl font-semibold text-shadow'>{title}</h3>
+        </div>
+        <div>
+          <img class='w-full h-48 object-cover mb-2' src={url} alt={title} />
+          <p>{id}</p>
+        </div>
+      </Link>
     </div>
   );
 }
